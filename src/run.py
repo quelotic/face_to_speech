556b63968b9ab95668fafbb86751487b23277391
Thomas Antoniou <quelotic@users.noreply.github.com>
2017-09-03 12:54:30 +0300

    Add files via upload

--- src/run.py
+++ src/run.py
@@ -8,62 +8,123 @@ timer = time.time()
 alexTimer = time.time()-7
 thomasTimer = time.time()-7
 theoTimer = time.time()-7
+chrisPTimer = time.time()-7
+chrisATimer = time.time()-7
+nasiaTimer = time.time()-7
 cdname = None
+listings = []
+
 
 def shoutAlexander(label):
     global cdname, alexTimer
     if cdname == label and time.time()-alexTimer > 10:
-        #os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/alexander.wav')
-        os.system('rosrun sound_play say.py "Hello, ' + label + '!"')
+        os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/alexander.wav')
         cdname = label
         alexTimer = time.time()
     elif cdname != label:
-        #os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/alexander.wav')
-        os.system('rosrun sound_play say.py "Hello, ' + label + '!"')
+        os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/alexander.wav')
         cdname = label
         alexTimer = time.time()
 
 def shoutThomas(label):
     global cdname, thomasTimer
     if cdname == label and time.time()-thomasTimer > 10:
-        #os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/thomas.wav')
-        os.system('rosrun sound_play say.py "Hello, ' + label + '!"')
+        os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/thomas.wav')
         cdname = label
         thomasTimer = time.time()
     elif cdname != label:
-        #os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/thomas.wav')
-        os.system('rosrun sound_play say.py "Hello, ' + label + '!"')
+        os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/thomas.wav')
         cdname = label
         thomasTimer = time.time()
 
 def shoutTheodore(label):
     global cdname, thomasTimer
     if cdname == label and time.time()-theoTimer > 10:
-        #os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/theodore.wav')
-        os.system('rosrun sound_play say.py "Hello, ' + label + '!"')
+        os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/theodore.wav')
         cdname = label
         theoTimer = time.time()
     elif cdname != label:
-        #os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/theodore.wav')
-        os.system('rosrun sound_play say.py "Hello, ' + label + '!"')
+        os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/theodore.wav')
         cdname = label
         theoTimer = time.time()
 
-def callback(msg):
-    global timer
+def shoutChrisA(label):
+    global cdname, chrisATimer
+    if cdname == label and time.time()-chrisATimer > 10:
+        os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/chris.wav')
+        cdname = label
+        chrisATimer = time.time()
+    elif cdname != label:
+        os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/chris.wav')
+        cdname = label
+        chrisATimer = time.time()
+
+def shoutChrisP(label):
+    global cdname, chrisPTimer
+    if cdname == label and time.time()-chrisPTimer > 10:
+        os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/chris.wav')
+        cdname = label
+        chrisPTimer = time.time()
+    elif cdname != label:
+        os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/chris.wav')
+        cdname = label
+        chrisPTimer = time.time()
+
+def shoutNasia(label):
+    global cdname, nasiaTimer
+    if cdname == label and time.time()-nasiaTimer > 10:
+        os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/nasia.wav')
+        cdname = label
+        nasiaTimer = time.time()
+    elif cdname != label:
+        os.system('rosrun sound_play play.py /home/quelotic/catkin_ws/src/face_to_speech/sounds/nasia.wav')
+        cdname = label
+        nasiaTimer = time.time()
+
+def appendToList(label):
+    global listings
+    listings.append(label)
+
+def checkEqual(iterator):
+    iterator = iter(iterator)
     try:
-        if time.time()-timer > 2:
+        first = next(iterator)
+    except StopIteration:
+        return True
+    return all(first == rest for rest in iterator)
+
+def callback(msg):
+    global timer, list
+    try:
+        if time.time()-timer > 0.5:
             label = msg.detections[0].label
-            print (label)
+            appendToList(label)
+            print (listings)
             timer = time.time()
-            if label == 'Thomas':
-                shoutThomas(label)
-            elif label == 'Alexander':
-                shoutAlexander(label)
-            elif label == 'Theodore':
-                shoutTheodore(label)
-            else:
-                pass
+            equal = checkEqual(listings)
+            if not equal:
+                del listings[:]
+            if len(listings) == 6 and equal:
+                if label == 'Thomas':
+                    shoutThomas(label)
+                    del listings[:]
+                elif label == 'Alexander':
+                    shoutAlexander(label)
+                    del listings[:]
+                elif label == 'Theodore':
+                    shoutTheodore(label)
+                    del listings[:]
+                elif label == 'ChrisA':
+                    shoutChrisA(label)
+                    del listings[:]
+                elif label == 'ChrisP':
+                    shoutChrisP(label)
+                    del listings[:]
+                elif label == 'Nasia':
+                    shoutNasia(label)
+                    del listings[:]
+            #elif len(listings) == 6:
+            #    del listings[:]
     except:
         pass
 
